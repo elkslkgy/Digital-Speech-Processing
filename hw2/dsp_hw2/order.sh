602ac8a15b2ab303fcdb0b7a122b5677f7b7e9e9
elkslkgy <b05902118@ntu.edu.tw>
2021-01-17 16:33:07 +0800

    Delete hw2/dsp_hw2 directory

--- hw2/dsp_hw2/order.sh
+++
@@ -1,5 +0,0 @@
-sh 00_clean_all.sh
-sh 01_run_HCopy.sh
-sh 02_run_HCompV.sh
-sh 03_training.sh
-sh 04_testing.sh
--- hw2/dsp_hw2/set_htk_path.sh
+++
@@ -1,1 +0,0 @@
-PATH=$PATH:"/Users/evan/Documents/university/Digital_Speech_Processing/dsp_hw2/dsp_hw2/htk341_debian_x86_64"
