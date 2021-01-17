9b339c81e9b1aa4cab627b83847aca99595de7c6
elkslkgy <b05902118@ntu.edu.tw>
2021-01-17 16:20:49 +0800

    Add files via upload

---
+++ hw2/dsp_hw2/order.sh
@@ -0,0 +1,5 @@
+sh 00_clean_all.sh
+sh 01_run_HCopy.sh
+sh 02_run_HCompV.sh
+sh 03_training.sh
+sh 04_testing.sh
---
+++ hw2/dsp_hw2/set_htk_path.sh
@@ -0,0 +1,1 @@
+PATH=$PATH:"/Users/evan/Documents/university/Digital_Speech_Processing/dsp_hw2/dsp_hw2/htk341_debian_x86_64"
